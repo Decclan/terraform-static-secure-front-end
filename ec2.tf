a9c8466bf2c16edf90c2179871ccc82f7115bf8b
Decclan <dec.evan11@gmail.com>
2022-03-05 09:20:58 +0000

    first

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIAUAZTG4LVVVFW32BZ"
-  secret_key = "OmCdbrQAla7bIkrvQhx4klhxT99HeGGfIghKzZn1"
-  region     = "eu-west-1"
-}
-
 resource "aws_instance" "example-ec2-1" {
   ami           = "ami-080af029940804103"
   instance_type = "t2.micro"
